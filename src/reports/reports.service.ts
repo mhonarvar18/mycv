e504548af6ad89170385a73e7cb2f17d675a8586
Mohammad Honarvar <mhonarvar18@gmail.com>
2023-04-09 15:59:21 +0330

    Add api for create report (basic)

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Report } from './report.entity';
+import { Repository } from 'typeorm';
+import { CreateReportDto } from './dtos/create-report.dto';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(
+    @InjectRepository(Report) private reportRepository: Repository<Report>
+  ){}
+
+  async create(reportDto: CreateReportDto) {
+    const report = this.reportRepository.create(reportDto);
+    return this.reportRepository.save(report);
+  }
+}
