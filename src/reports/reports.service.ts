4aee63c98163aae7e7561042f294df743b4ffc73
Mohammad Honarvar <mhonarvar18@gmail.com>
2023-04-29 10:26:42 +0330

    Add relation between user and report

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -3,15 +3,17 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Report } from './report.entity';
 import { Repository } from 'typeorm';
 import { CreateReportDto } from './dtos/create-report.dto';
+import { User } from '@src/users/user.entity';
 
 @Injectable()
 export class ReportsService {
   constructor(
     @InjectRepository(Report) private reportRepository: Repository<Report>
-  ){}
+  ) { }
 
-  async create(reportDto: CreateReportDto) {
+  async create(reportDto: CreateReportDto, user: User) {
     const report = this.reportRepository.create(reportDto);
+    report.user = user;
     return this.reportRepository.save(report);
   }
 }
